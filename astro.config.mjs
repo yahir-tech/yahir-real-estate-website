868cd9ff79d6b68cb7df769de69468ffdc44326b
yahir-tech <yahir@bigrealty.com>
2026-01-14 14:29:30 -0600

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    integrations: [react()],
+    adapter: netlify({
+        devFeatures: {
+            environmentVariables: true
+        }
+    })
+});
